1e2273be4c47567e9a373b3af24aa1a549335c2f
LeoKamimuraLarsson <92987067+LeoKamimuraLarsson@users.noreply.github.com>
2023-02-15 12:01:12 +0100

    Tog bort risker för SQL-injection

--- app.rb
+++ app.rb
@@ -18,6 +18,9 @@ end
 
 post("/games") do # lägg nytt spel
     name = params[:name]
+
+    # kolla om namnet är tomt
+
     db_insert_one_into("Game", "name", name)
     redirect("/")
 end
@@ -27,8 +30,8 @@ get("/games/:id") do # visa ett spel
     
     #@result = db_innerjoin_two("Game.name", "Category.*", "Game", "Category", "Game.id", "Category.game_id", "Game.id", game_id)
     
-    @game = db_get_one_equal("Game", "*", "id", game_id).first
-    @categories = db_get_one_equal("Category", "*", "game_id", game_id) 
+    @game = db_get_all_equal("Game", "id", game_id).first
+    @categories = db_get_all_equal("Category", "game_id", game_id) 
 
     slim(:"games/show")
 end
@@ -37,19 +40,22 @@ post("/games/:id/update") do
     game_id = params[:id]
     new_name = params[:new_name]
 
-    db_update_condition("Game", "name = \'#{new_name}\'", "id = #{game_id}")
+    db_update_condition("Game", "name", new_name, "id", game_id)
+    #db_update_condition("Game", "name = \'#{new_name}\'", "id = #{game_id}")
     redirect("/games/#{game_id}")
 end
 
 post("/games/:id/delete") do
     game_id = params[:id]
-    db_delete("Game", "id = #{game_id}")
+    db_delete("Game", "id", game_id)
     redirect("/games")
 end
 
 post("/categories") do # lägg till ny kategori
     name = params[:name]
     game_id = params[:game_id]
+
+    # kolla om namnet är tomt
 
     db_insert_into("Category", "name, game_id", name, game_id)
     redirect("/games/#{game_id}")
@@ -59,11 +65,11 @@ get("/categories/:id") do
     cat_id = params[:id]
 
     @articles = db_get_articles_in_category(cat_id)
-    @categories = db_get_one_equal("Category", "*", "id", cat_id).first
+    @categories = db_get_all_equal("Category", "id", cat_id).first
     
     game_id = @categories["game_id"]
-    @game = db_get_one_equal("Game", "*", "id", game_id).first
-    p @game
+    @game = db_get_all_equal("Game", "id", game_id).first
+    #p @game
     
 
     slim(:"categories/show")
--- model.rb
+++ model.rb
@@ -28,49 +28,19 @@ def db_get_all(table)
     return db.execute("SELECT * FROM #{table}")
 end
 
+def db_get_all_equal(table, compare_attribute, variable) 
+    db = connect_to_mdb()
+    return db.execute("SELECT * FROM #{table} WHERE #{compare_attribute} = ?", variable)
+end
+
 def db_get_one(table, attribute)
     db = connect_to_mdb()
     return db.execute("SELECT #{attribute} FROM #{table}")
 end
 
-def db_get_one_equal(table, attribute, a, b) 
+def db_get_one_equal(table, attribute, compare_attribute, variable) 
     db = connect_to_mdb()
-    return db.execute("SELECT #{attribute} FROM #{table} WHERE #{a} = #{b}")
-end
-
-def db_get_many_equal(table, attribute, array1, array2) 
-    db = connect_to_mdb()
-
-    if array1.length != array2.length 
-        raise "Array1 och array2 har inte lika många element"
-    elsif array1.length == 0
-        raise "Arrays är tomma"
-    end
-
-    condition = ""
-
-    i = 0
-    while i < array1.length
-
-        condition += "#{array1[i]} = #{array2[i]}"
-
-        if i < array1.length - 1 # om inte sista elementet
-            condition += " AND "
-        end
-
-        i += 1
-    end
-    
-    return db.execute("SELECT #{attribute} FROM #{table} WHERE #{condition}}")
-end
-
-def db_innerjoin_two(attribute1, attribute2, table1, table2, on1, on2, on3, on4, a, b)
-    db = connect_to_mdb_array()
-    return db.execute("SELECT #{attribute1}, #{attribute2} 
-        FROM #{table1}
-        INNER JOIN #{table2}
-        ON #{on1} = #{one2} AND #{on3} = #{on4}
-        WHERE #{a} = #{b}")
+    return db.execute("SELECT #{attribute} FROM #{table} WHERE #{compare_attribute} = ?", variable)
 end
 
 def db_get_articles_in_category(category_id)
@@ -80,7 +50,7 @@ def db_get_articles_in_category(category_id)
         FROM Category
         INNER JOIN Article, Article_Category_Relation
         ON Category.id = Article_Category_Relation.category_id AND Article.id = Article_Category_Relation.article_id
-        WHERE Category.id = #{category_id}")
+        WHERE Category.id = ?", category_id)
 end
 
 # Insert into database
@@ -104,14 +74,14 @@ end
 
 # Update database
 
-def db_update_condition(table, set, condition)
+def db_update_condition(table, set_attribute, set_variable, compare_attribute, compare_variable)
     db = connect_to_mdb()
-    db.execute("UPDATE #{table} SET #{set} WHERE #{condition}")
+    db.execute("UPDATE #{table} SET #{set_attribute} = ? WHERE #{compare_attribute} = ?", set_variable, compare_variable)
 end
 
 # Delete from database
 
-def db_delete(table, condition)
+def db_delete(table, compare_attribute, variable)
     db = connect_to_mdb_array()
-    db.execute("DELETE FROM #{table} WHERE #{condition}")
+    db.execute("DELETE FROM #{table} WHERE #{compare_attribute} = ?", variable)
 end
